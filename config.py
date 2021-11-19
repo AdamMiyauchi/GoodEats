602cba7dedc9341c7e653a8ae6f8e0db2a6a9f38
Adam Miyauchi <adammiyauchi@Adams-MacBook-Pro.local>
2021-11-18 17:15:39 -0700

    ui

---
+++ config.py
@@ -0,0 +1,7 @@
+
+# Heroku Postgres Stuff
+host = 'ec2-23-23-162-138.compute-1.amazonaws.com'
+database = 'd8kf9t5gip0jrr'
+user = 'ztktpaolkllvqd'
+port = '5432'
+password = 'dd1abc08af6b375888b5d78aa4a002b48ae79c61dc999fc9683d5331ebe879f5'
